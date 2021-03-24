c0b48956540dd51329c14e02f7fceb9113b4c70a
MuuKojima <muu.kojima.dev@gmail.com>
2021-03-24 14:47:44 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const parse = require('csv-parse');
+const readable = fs.createReadStream('input.csv');
+
+const parser = parse({
+  columns: true,
+  from_line: 1
+});
+
+readable
+  .pipe(parser)
+  .on('data', (data) => {
+    console.log('pauseの前', data);
+    readable.pause();
+    console.log('pauseの後');
+    setTimeout(() => {
+      console.log('resumeの前');
+      readable.resume();
+    }, 3000);
+  })
+  .on('end', () => {
+    console.log('end');
+  });
