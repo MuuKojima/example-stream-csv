7d92fd9b6c7ba92bd5218619447301614b90b8eb
MuuKojima <muu.kojima.dev@gmail.com>
2021-03-26 17:57:36 +0900

    Update Stream

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 
-const request = async (obj) => {
+const fetchSomeWhere = async item => {
   await new Promise(resolve => {
     setTimeout(() => {
-      console.log('📤', obj);
+      console.log('📤', item);
       resolve();
     }, 1000)
   });
 };
 
-const readCsv = async () => {
+const readCsvBySimpleStream = async () => {
+  return await new Promise((resolve) => {
+    results = [];
+    const readable = fs.createReadStream('input.csv');
+    const parser = parse({
+      columns: true,
+      from_line: 1
+    });
+    readable
+      .pipe(parser)
+      .on('data', async (data) => {
+        parser.pause();
+        await fetchSomeWhere(data);
+        results.push(data);
+        parser.resume();
+      })
+      .on('end', () => {
+        resolve(results);
+      })
+  });
+}
+
+const readCsvByAsyncIterator = async () => {
   results = [];
   const parser = fs
     .createReadStream('./input.csv')
@@ -18,14 +40,23 @@ const readCsv = async () => {
       columns: true,
       from_line: 1
     }));
-  for await (const record of parser) {
-    results.push(record);
-    await request(record);
+  for await (const item of parser) {
+    await fetchSomeWhere(item);
+    results.push(item);
   }
   return results;
 }
 
+// 単純なStreamだとendの呼ばれるタイミングが、dataを全部読み込んだ `瞬間` に発火するので、
+// 結果的に、最後の一個が入ってないので、扱いずらいかも。
+// ホントはdataイベントが発生して、dataイベント内のコードが全部実行されてから、endにいってほしい、、
 (async () => {
-  const results = await readCsv();
-  console.info('✅', results);
+  const results = await readCsvBySimpleStream();
+  console.info('✅ Stream', results);
+})();
+
+// Stream Async Iterator
+(async () => {
+  const results = await readCsvByAsyncIterator();
+  console.info('✅ Async Iterator', results);
 })();
